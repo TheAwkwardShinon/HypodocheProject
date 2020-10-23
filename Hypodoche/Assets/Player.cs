f768b96c9fcd33f30cc1f768744072850ab94b08
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2020-10-23 17:16:33 +0200

    Basic player movements and simple arena
    player movement (only for AI boss testing)
    simple arena for testing.

---
+++ Hypodoche/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rigidBody.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+
+    }
+}
