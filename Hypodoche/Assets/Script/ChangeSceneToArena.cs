3147e5a9b14fce7fb35488c910f6113ae5baccfb
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2020-12-05 03:30:59 +0100

    added menu and musics, minor bug fix

---
+++ Hypodoche/Assets/Script/ChangeSceneToArena.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Hypodoche
+{
+    public class ChangeSceneToArena : MonoBehaviour
+    {
+        public void playGame()
+        {
+            SceneManager.LoadScene("ArenaScene");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
